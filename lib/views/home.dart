07f19772f51672045b959c9b6b7cdc7fcbd226ec
shubham3886 <55235096+shubham3886@users.noreply.github.com>
2020-04-04 20:32:42 +0530

    Initial commit

---
+++ lib/views/home.dart
@@ -0,0 +1,48 @@
+import 'package:blog_project/views/create_blog.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter",
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              "Blog",
+              style: TextStyle(fontSize: 22, color: Colors.blue),
+            )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Container(),
+      floatingActionButton: Container(
+        padding: EdgeInsets.symmetric(vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CreateBlog()));
+              },
+              child: Icon(Icons.add),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
